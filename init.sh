69a1d008d2a738b39cfaab788dd0405df37cefab
concrete-cristian-trucco <cristian.trucco@concrete.com.br>
2018-09-10 22:30:32 -0300

    alterado script init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 PASSWORD='root'
 docker-compose up -d
-#cd  mysql/scripts/database_schema.sql
+
+## comando para criar o banco e a tabela que será usada pela aplicação
+
 docker exec -i mysql mysql -u root -p${PASSWORD} -h localhost notes < database_schema.sql 2> /dev/null
+
+echo "Listando Containers criados"
+
+docker-compose ps
+
+echo "Inserindo massa de teste"
+
+curl -X POST http://localhost/notes --data {"Text":"Desafio"}
+curl -X POST http://localhost/notes --data {"Text":"Passei"}
+curl -X POST http://localhost/notes --data {"Text":"Direto"}
+
