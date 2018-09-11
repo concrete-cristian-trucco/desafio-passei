17cf1f023ec1ace25d24a1a9ad67b7b29a3e0d96
concrete-cristian-trucco <cristian.trucco@concrete.com.br>
2018-09-10 22:18:30 -0300

    alterado script init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PASSWORD='root'
-docker-compose up 
+docker-compose up -d
 #cd  mysql/scripts/database_schema.sql
 docker exec -i mysql mysql -u root -p${PASSWORD} -h localhost notes < database_schema.sql 2> /dev/null
