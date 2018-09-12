eea7d43d63e2c074fe8eef28cfef390b3eb7f794
concrete-cristian-trucco <cristian.trucco@concrete.com.br>
2018-09-12 16:18:18 -0300

    terraform docker provider

--- terraform/docker-desafio.tf
+++ terraform/docker-desafio.tf
@@ -1,6 +1,11 @@
 # Configure the Docker provider
 provider "docker" {
-  host = "tcp://127.0.0.1:2376/"
+  host = "unix:///var/run/docker.sock"
+}
+
+# Create a new docker network
+resource "docker_network" "private_network" {
+  name = "desafio-passei-direto"
 }
 
 # Create a container
@@ -11,7 +16,7 @@ resource "docker_container" "application" {
     internal = 8080
     external = 8080
   }
-  networks = ["desafio-passei-direto"]
+  networks = [  "${docker_network.private_network.name}" ]
   restart = "on-failure"
   links = ["mysql"]
 }
@@ -28,7 +33,7 @@ resource "docker_container" "nginx" {
     internal = 80
     external = 80
   }
-  networks = ["desafio-passei-direto"]
+  networks = [  "${docker_network.private_network.name}" ]
   restart = "always"
   links = ["application"]
 }
@@ -43,13 +48,30 @@ resource "docker_container" "mysql" {
     internal = 3306
     external = 3306
   }
-  networks = ["desafio-passei-direto"]
-}
+  networks = [  "${docker_network.private_network.name}" ]
+  volumes = {
+    volume_name    = "db_data"
+    container_path = "/data"    
+  }
 
+  # Copies all files and folders in apps/app1 to D:/IIS/webapp1
+
+#provisioner "file" {
+#    source      = "database_schema.sql"
+#    destination = "/data"
+#  }
+
+provisioner "remote-exec" {
+             connection {
+                 user = "root"
+                 password = "root"
+             }
+        inline = [
+           "mysql -u root -p root -h localhost notes < database_schema.sql 2> /dev/null"
+        ]
+    }
+}
 resource "docker_image" "mysql" {
   name = "concretecristiantrucco/desafio_mysql:1.0"
 }
 
-resource "docker_network" "desafio-passei-direto" {
-  name = "desafio-passei-direto"
-}
